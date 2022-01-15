ebab9fbc458bf707829ea264b7bbcc762e5084be
Saniya Nafees <saniyanafees6@gmail.com>
2022-01-15 12:26:04 -0700

    added redux logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 // import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
-
+import { Provider } from 'react-redux';
+import { store } from './state';
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
